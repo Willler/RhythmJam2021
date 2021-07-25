a64f81e3f548fc05c08e843918c972d788ee4437
Gabrielle Poirier <gaby.poirier@hotmail.com>
2021-07-24 21:03:59 -0400

    Jukebox script
    Started working on the Jukebox script which would handle all the music.

--- RhythmJam/Assets/Scripts/Audio/Jukebox.cs
+++ RhythmJam/Assets/Scripts/Audio/Jukebox.cs
@@ -4,15 +4,114 @@ using UnityEngine;
 
 public class Jukebox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Jukebox")]
+    [SerializeField] private bool m_TrackIsPlaying = false; //Checks if a track is playing.
+    [SerializeField] private int m_CurrentTrack = 0;        //Indicates which track is playing.
+    [SerializeField] private int m_BPM = 0;                 //The BPM/tempo of the song.
+
+    [Header("Tracks")]
+    [SerializeField] private bool[] m_AllTracks;            //Bool to check which track is playing.
+    
+
+    private static Jukebox instance;
+
+    public static Jukebox Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Jukebox>();
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    singleton.AddComponent<Jukebox>();
+                    singleton.name = "(Singleton) Jukebox";
+                }
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //Each of the following integers corresponds to a track that can be played. 
+    //0 - Title Screen
+    //1 - Track 1
+    //2 - Track 2
+    //3 - Track 3
+
+    void Start()
+    {
+        Instance.PlayTrack(0);
+    }
+
     void Update()
     {
-        
+
+    }
+
+    void PlayTrack(int Track)
+    {
+        if (Track == 0)
+        {
+            //StopTrack();
+            m_CurrentTrack = 0;
+            m_AllTracks[m_CurrentTrack] = true;
+            m_BPM = 0;
+        }
+        else if (Track == 1 && m_CurrentTrack != 1)
+        {
+            //StopTrack();
+            //SoundManager.Instance.BGM_Start("m_BGM_TitleScreen");
+            m_CurrentTrack = 1;
+            m_AllTracks[m_CurrentTrack] = true;
+            m_BPM = 120;        //Change to whatever the actual BPM is once we get to it.
+        }
+        else if (Track == 2 && m_CurrentTrack != 2)
+        {
+            //StopTrack();
+            //SoundManager.Instance.BGM_Start("m_BGM_Exploration_Neutral");
+            m_CurrentTrack = 2;
+            m_AllTracks[m_CurrentTrack] = true;
+            m_BPM = 120;        //Change to whatever the actual BPM is once we get to it.
+        }
+        else if (Track == 3 && m_CurrentTrack != 3)
+        {
+            //StopTrack();
+            //SoundManager.Instance.BGM_Start("m_BGM_Exploration_Eerie");
+            m_CurrentTrack = 3;
+            m_AllTracks[m_CurrentTrack] = true;
+            m_BPM = 120;        //Change to whatever the actual BPM is once we get to it.
+        }
+
+        m_TrackIsPlaying = true;
+    }
+
+    void StopTrack()
+    {
+        if (m_CurrentTrack == 0)
+        {
+            //SoundManager.Instance.BGM_Stop("m_BGM_TitleScreen_Stop");
+            m_AllTracks[0] = false;
+        }
+        else if (m_CurrentTrack == 1)
+        {
+            //SoundManager.Instance.BGM_Stop("m_BGM_Exploration_Neutral_Stop");
+            m_AllTracks[1] = false;
+        }
+        else if (m_CurrentTrack == 2)
+        {
+            //SoundManager.Instance.BGM_Stop("m_BGM_Exploration_Eerie_Stop");
+            m_AllTracks[2] = false;
+        }
+        else if (m_CurrentTrack == 3)
+        {
+            //SoundManager.Instance.BGM_Stop("m_BGM_Exploration_Eerie_Stop");
+            m_AllTracks[3] = false;
+        }
     }
 }
