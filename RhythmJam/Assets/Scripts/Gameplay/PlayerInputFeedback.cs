cc91e1f403b3915676f6f0ccdbe71a66414089b2
William L'Eriger <espermentalout@gmail.com>
2021-07-25 01:06:08 -0400

    Hitting Notes Functionality

---
+++ RhythmJam/Assets/Scripts/Gameplay/PlayerInputFeedback.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputFeedback : MonoBehaviour
+{
+
+    private SpriteRenderer playerSR;
+    public Sprite defaultSprite;
+    public Sprite inputSprite;
+
+    public KeyCode keyForNoteInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyForNoteInput))
+        {
+            playerSR.sprite = inputSprite;
+        } 
+
+        if (Input.GetKeyUp(keyForNoteInput))
+        {
+            playerSR.sprite = defaultSprite;
+        }
+    }
+}
