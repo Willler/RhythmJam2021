865f0201a90528331fbaef6831d7e621336ffc30
William L'Eriger <espermentalout@gmail.com>
2021-07-27 01:07:11 -0400

    tweaking animation, note rotations

---
+++ RhythmJam/Assets/Scripts/Gameplay/NoteRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject note;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        note.transform.Rotate(Vector3.forward * + 0.05f);
+    }
+}
